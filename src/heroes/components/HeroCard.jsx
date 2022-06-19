58658dae038281e12bcc6e108fe31bfd032cbd03
David <david.cnvs@gmail.com>
2022-06-19 15:56:31 -0400

    primer commit

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,27 @@
+
+export const HeroCard = ({
+    id,
+    superhero, 
+    publisher, 
+    alter_ego,
+    first_appearance,
+    characters
+}) => {
+
+    const heroImageUrl = ``;
+  
+    return (
+    <div className="col">
+        <div className="card">
+            <div className="row no-gutters">
+                <div className="col-4">
+                    <img src="" alt={superhero} className="card-img" />
+                </div>
+            </div>
+            <div className="row no-gutters">
+
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/ui/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+
+export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark ps-4 pe-4">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+                        className={isActive =>
+                            "nava-item nav-link" + (!isActive ? " unselected" : "")
+                          } 
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        className={isActive =>
+                            "nava-item nav-link" + (!isActive ? " unselected" : "")
+                          }  
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+                    <span className='nav-item nav-link text-info'>
+                        David
+                    </span>
+                    <button 
+                    className='nav-item nav-link btn'
+                    onClick={handleLogout}
+                    >
+                          Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
