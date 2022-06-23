b9dbc11e14c4427f42d7e63121b2f1b1ae973a48
David <david.cnvs@gmail.com>
2022-06-23 10:06:32 -0400

    wip proteccion rutas

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./authReducer"
+import { types } from "../types/types"
+
+const initialSate = {
+    logged: false
+}
+
+export const AuthProvider = ({children}) => {
+
+    const [authState, dispatch] = useReducer(authReducer, initialSate);
+
+    const login = (name = '') => {
+        const action = {
+            types: types.login,
+            payload: {
+                id: 'ABC',
+                name: name
+            }
+        }
+        dispatch(action);
+    }
+
+  return (
+    <AuthContext.Provider value={{
+        ...authState,
+        login: login
+    }}>
+        {children}
+    </AuthContext.Provider>
+  );
+}
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
 
+  const {login} = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    navigate('/marvel', {
+
+    login('Dave');
+
+    navigate('/', {
       replace: true
     });
   }
